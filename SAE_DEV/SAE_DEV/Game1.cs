3ab8f346e7be12bd1a75e0ed8f93a0c878d96998
lakouisy <lakouisy@D350-S18>
2023-01-03 17:40:21 +0100

    ajout du zombie

--- SAE_DEV/SAE_DEV/Game1.cs
+++ SAE_DEV/SAE_DEV/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
 
 namespace SAE_DEV
 {
@@ -9,6 +12,10 @@ namespace SAE_DEV
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private SpriteBatch test;
+        private Vector2 _positionZombie;
+        private AnimatedSprite _Zombielvl1;
+        private Vector2 _positionPerso;
+        private AnimatedSprite _perso;
 
         public Game1()
         {
@@ -22,13 +29,18 @@ namespace SAE_DEV
             // TODO: Add your initialization logic here
 
             Window.Title = "Test";
-
+            _positionZombie = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+            _positionPerso = new Vector2(GraphicsDevice.Viewport.Width / 2 + 50, GraphicsDevice.Viewport.Height / 2);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("ZombieToast_50.sf", new JsonContentLoader());
+            _Zombielvl1 = new AnimatedSprite(spriteSheet);
+            SpriteSheet spritePerso = Content.Load<SpriteSheet>("FinnSprite.sf", new JsonContentLoader());
+            _perso = new AnimatedSprite(spritePerso);
 
             // TODO: use this.Content to load your game content here
         }
@@ -37,8 +49,13 @@ namespace SAE_DEV
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             // TODO: Add your update logic here
+            _Zombielvl1.Play("idle");
+            _Zombielvl1.Update(deltaTime);
+            _perso.Play("idle");
+            _perso.Update(deltaTime);
+           
 
             base.Update(gameTime);
         }
@@ -47,8 +64,15 @@ namespace SAE_DEV
         {
             GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
 
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_Zombielvl1, _positionZombie);
+            _spriteBatch.End();
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_perso, _positionPerso);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
