2af269f49f9daf374e5d897a1bcae6b7c53625e6
martmate <martmate@D350-S21>
2023-01-04 15:23:23 +0100

    1ere map

--- SAE_DEV/SAE_DEV/Game1.cs
+++ SAE_DEV/SAE_DEV/Game1.cs
@@ -33,7 +33,7 @@ namespace SAE_DEV
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
-
+            
         protected override void Initialize()
         {
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
