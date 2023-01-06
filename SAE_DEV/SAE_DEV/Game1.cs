0d4f92a9aa851334fa75cdc17ec46c67c9fd4608
lakouisy <lakouisy@D350-S18>
2023-01-06 08:52:27 +0100

    fusion

--- SAE_DEV/SAE_DEV/Game1.cs
+++ SAE_DEV/SAE_DEV/Game1.cs
@@ -21,23 +21,11 @@ namespace SAE_DEV
     {
         public int MAP1_TAILLE = 800;
         public int MAP2_TAILLE = 560;
-        private TiledMapTileLayer mapLayer;
         private KeyboardState _keyboardState;
         private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-        private TiledMap _tiledMap;
-        private TiledMapRenderer _tiledMapRenderer;
-        private Vector2 _positionZombie;
-        private AnimatedSprite _Zombielvl1;
-        private Vector2 _positionPerso;
-        private int _vitessePerso;
-        private AnimatedSprite _perso;
-        private string _animationPerso;
-        private OrthographicCamera _camera;
-        private float _positionCameraX;
-        private float _positionCameraY;
         private int _screenWidth;
         private int _screenHeight;
+        private TiledMapTileLayer mapLayer;
         public SpriteBatch SpriteBatch { get; private set; }
 
         private ScreenManager _screenManager;
@@ -66,19 +54,12 @@ namespace SAE_DEV
 
             Window.Title = "Sae Dev";
 
-            _positionPerso = new Vector2(150, 250);
-            _vitessePerso = 150;
-            _positionZombie = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+            
             _screenManager = new ScreenManager();
             
 
             _screenWidth = 1280;
             _screenHeight = 720;
-
-            var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, _screenWidth, _screenHeight);
-            _camera = new OrthographicCamera(viewportAdapter);
-            _camera.Position = new Vector2(_screenWidth,_screenHeight);
-            _camera.ZoomIn(1.5f);
 
             base.Initialize();
         }
@@ -98,29 +79,9 @@ namespace SAE_DEV
             LoadScreen(new Monde(this));
         }
 
-        protected override void LoadContent()
-        {
-            
-
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("ZombieToast_50.sf", new JsonContentLoader());
-            _Zombielvl1 = new AnimatedSprite(spriteSheet);
-            SpriteSheet spritePerso = Content.Load<SpriteSheet>("FinnSprite.sf", new JsonContentLoader());
-            _perso = new AnimatedSprite(spritePerso);
-            SpriteBatch = new SpriteBatch(GraphicsDevice);
-
-            _tiledMap = Content.Load<TiledMap>("map2");
-            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
-            mapLayer = _tiledMap.GetLayer<TiledMapTileLayer>("Batiment");
-
-            // TODO: use this.Content to load your game content here
-            LoadMenu();
-        }
-
         protected override void Update(GameTime gameTime)
         {
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds; // TIME
-            float walkSpeed = deltaTime * _vitessePerso; // Vitesse de déplacement du sprite
             _keyboardState = Keyboard.GetState();
 
 
@@ -129,150 +90,24 @@ namespace SAE_DEV
                 Exit();
 
             
-
-
-            _Zombielvl1.Play("idle");
-            
-            _animationPerso = "idle";
-
-            _positionCameraX = _positionPerso.X;
-            _positionCameraY = _positionPerso.Y;
-
-            // En a gauche
-            if (_positionPerso.X < _screenWidth / 5)
-                _positionCameraX = _screenWidth / 5;
-            // A droite
-            if (_positionPerso.X > (MAP1_TAILLE - _screenWidth / 5))
-                _positionCameraX = (MAP1_TAILLE - _screenWidth / 5);
-            //en haut
-            if (_positionPerso.Y < _screenHeight / 5)
-                _positionCameraY = _screenHeight / 5;
-            // en bas
-            if (_positionPerso.Y > (MAP1_TAILLE - _screenHeight / 5))
-                _positionCameraY = (MAP1_TAILLE - _screenHeight / 5);
-
-
-
-            //Deplacement du perso + collisions
-            if (_keyboardState.IsKeyDown(Keys.Right))
-            {
-                ushort tx = (ushort)(_positionPerso.X/ _tiledMap.TileWidth +0.5);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileWidth);
-                _animationPerso = "running";
-                if (!IsCollision(tx, ty))
-                {
-                    _positionPerso.X += walkSpeed;
-                }
-            }
-            if (_keyboardState.IsKeyDown(Keys.Up))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileWidth -0.5);
-                _animationPerso = "running";
-                if (!IsCollision(tx, ty))
-                {
-                    _positionPerso.Y -= walkSpeed;
-                }
-                
-            }
-            if (_keyboardState.IsKeyDown(Keys.Down))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileWidth +0.5);
-                _animationPerso = "running";
-                if (!IsCollision(tx, ty))
-                {
-                    _positionPerso.Y += walkSpeed;
-                }
-                
-            }
-            if (_keyboardState.IsKeyDown(Keys.Left))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth -0.5);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileWidth);
-                _animationPerso = "running";
-                if (!IsCollision(tx, ty))
-                {
-                    _positionPerso.X -= walkSpeed;
-                }
-            }
-
-            _camera.LookAt(new Vector2(_positionCameraX, _positionCameraY));
-
-
-            _perso.Play(_animationPerso); // on joue l'animation du perso
-            _perso.Update(deltaTime); // temps écoulé
-            _tiledMapRenderer.Update(gameTime);
-            _Zombielvl1.Update(deltaTime);
-
-            
             
             _screenManager.Update(gameTime);
 
-            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            _graphics.GraphicsDevice.Clear(Color.Black);
-
-            var transformMatrix = _camera.GetViewMatrix();
-            
-            _spriteBatch.Begin(transformMatrix: transformMatrix);
-            
-            _tiledMapRenderer.Draw(transformMatrix);
             
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            _spriteBatch.Draw(_Zombielvl1, _positionZombie);
-             _spriteBatch.Draw(_perso, _positionPerso);
-            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             _screenManager.Draw(gameTime);
 
-
-
-
-
             base.Draw(gameTime);
-        }
-
-
-
-        private bool IsCollision(ushort x, ushort y)
-        {
-            TiledMapTile? tile;
-            if (mapLayer.TryGetTile(x,y, out tile) == false )
-                return false;
-            if (!tile.Value.IsBlank)
-                return true;
-            return false;
-        }
-
-        private Vector2 GetMovementDirection()
-        {
-            var movementDirection = Vector2.Zero;
-            var state = Keyboard.GetState();
-            if (state.IsKeyDown(Keys.Down))
-            {
-                movementDirection += Vector2.UnitY;
-            }
-            if (state.IsKeyDown(Keys.Up))
-            {
-                movementDirection -= Vector2.UnitY;
-            }
-            if (state.IsKeyDown(Keys.Left))
-            {
-                movementDirection -= Vector2.UnitX;
-            }
-            if (state.IsKeyDown(Keys.Right))
-            {
-                movementDirection += Vector2.UnitX;
-            }
-            return movementDirection;
         }
 
 
--- SAE_DEV/SAE_DEV/Screens/Monde.cs
+++ SAE_DEV/SAE_DEV/Screens/Monde.cs
@@ -10,6 +10,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MonoGame.Extended.ViewportAdapters;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Content;
 
 namespace SAE_DEV.Screens
 {
@@ -25,19 +30,26 @@ namespace SAE_DEV.Screens
         public int MAP2_TAILLE = 560;
         private TiledMap _tiledMap;
         private TiledMapRenderer _tiledMapRenderer;
+        private TiledMapTileLayer mapLayer;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Vector2 _positionZombie;
+        private AnimatedSprite _Zombielvl1;
+        private Vector2 _positionPerso;
+        private int _vitessePerso;
+        private AnimatedSprite _perso;
+        private string _animationPerso;
+        private OrthographicCamera _camera;
+        private float _positionCameraX;
+        private float _positionCameraY;
+        private int _screenWidth;
+        private int _screenHeight;
 
         public Monde(Game1 game) : base(game)
         {
 
         }
 
-        public override void LoadContent()
-        {
-            _tiledMap = Content.Load<TiledMap>("map1");
-            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
-            zombielvl1.LoadContent(Game);
-            base.LoadContent();
-        }
 
         public override void Initialize()
         {
@@ -47,14 +59,48 @@ namespace SAE_DEV.Screens
             zombielvl1 = new Zombie();
             zombielvl1.PositionZombie = new Vector2(300, 400);
             iazombie = new IAZombie(75, character, zombielvl1);
+            
+            var viewportAdapter = new BoxingViewportAdapter(Game.Window, GraphicsDevice, _screenWidth, _screenHeight);
+            _camera = new OrthographicCamera(viewportAdapter);
+            _camera.Position = new Vector2(_screenWidth, _screenHeight);
+            _camera.ZoomIn(1.5f);
+
+
+            _positionPerso = new Vector2(150, 250);
+            _vitessePerso = 150;
+            _positionZombie = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+
+
             base.Initialize();
+        }
+        public override void LoadContent()
+        {
+            _tiledMap = Content.Load<TiledMap>("map1");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            _tiledMap = Content.Load<TiledMap>("map2");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            mapLayer = _tiledMap.GetLayer<TiledMapTileLayer>("Batiment");
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("ZombieToast_50.sf", new JsonContentLoader());
+            _Zombielvl1 = new AnimatedSprite(spriteSheet);
+            SpriteSheet spritePerso = Content.Load<SpriteSheet>("FinnSprite.sf", new JsonContentLoader());
+            _perso = new AnimatedSprite(spritePerso);
+
+            zombielvl1.LoadContent(Game);
+
+            base.LoadContent();
         }
 
         public override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
 
-            _tiledMapRenderer.Draw();
+            var transformMatrix = _camera.GetViewMatrix();
+
+            _spriteBatch.Begin(transformMatrix: transformMatrix);
+
+            _tiledMapRenderer.Draw(transformMatrix);
 
             character.Draw(Game.SpriteBatch);
             zombielvl1.Draw(Game.SpriteBatch);
@@ -63,6 +109,82 @@ namespace SAE_DEV.Screens
         public override void Update(GameTime gameTime)
         {
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float walkSpeed = deltaTime * _vitessePerso;
+
+            _Zombielvl1.Play("idle");
+
+            _animationPerso = "idle";
+
+            _positionCameraX = _positionPerso.X;
+            _positionCameraY = _positionPerso.Y;
+
+            // En a gauche
+            if (_positionPerso.X < _screenWidth / 5)
+                _positionCameraX = _screenWidth / 5;
+            // A droite
+            if (_positionPerso.X > (MAP1_TAILLE - _screenWidth / 5))
+                _positionCameraX = (MAP1_TAILLE - _screenWidth / 5);
+            //en haut
+            if (_positionPerso.Y < _screenHeight / 5)
+                _positionCameraY = _screenHeight / 5;
+            // en bas
+            if (_positionPerso.Y > (MAP1_TAILLE - _screenHeight / 5))
+                _positionCameraY = (MAP1_TAILLE - _screenHeight / 5);
+
+
+
+            //Deplacement du perso + collisions
+            if (_keyboardState.IsKeyDown(Keys.Right))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 0.5);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileWidth);
+                _animationPerso = "running";
+                if (!IsCollision(tx, ty))
+                {
+                    _positionPerso.X += walkSpeed;
+                }
+            }
+            if (_keyboardState.IsKeyDown(Keys.Up))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileWidth - 0.5);
+                _animationPerso = "running";
+                if (!IsCollision(tx, ty))
+                {
+                    _positionPerso.Y -= walkSpeed;
+                }
+
+            }
+            if (_keyboardState.IsKeyDown(Keys.Down))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileWidth + 0.5);
+                _animationPerso = "running";
+                if (!IsCollision(tx, ty))
+                {
+                    _positionPerso.Y += walkSpeed;
+                }
+
+            }
+            if (_keyboardState.IsKeyDown(Keys.Left))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 0.5);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileWidth);
+                _animationPerso = "running";
+                if (!IsCollision(tx, ty))
+                {
+                    _positionPerso.X -= walkSpeed;
+                }
+            }
+
+            _camera.LookAt(new Vector2(_positionCameraX, _positionCameraY));
+
+
+            _perso.Play(_animationPerso); // on joue l'animation du perso
+            _perso.Update(deltaTime); // temps écoulé
+            _tiledMapRenderer.Update(gameTime);
+            _Zombielvl1.Update(deltaTime);
+
             _tiledMapRenderer.Update(gameTime);
             _keyboardState = Keyboard.GetState();
             character.Update(deltaTime);
@@ -73,6 +195,15 @@ namespace SAE_DEV.Screens
                 Game.LoadMenu();
             }
             
+        }
+        private bool IsCollision(ushort x, ushort y)
+        {
+            TiledMapTile? tile;
+            if (mapLayer.TryGetTile(x, y, out tile) == false)
+                return false;
+            if (!tile.Value.IsBlank)
+                return true;
+            return false;
         }
     }
 }
